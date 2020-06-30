16d44674b841c39545e9c8617161c78d82f304a8
李靖 <356055691@qq.com>
2020-06-30 14:12:31 +0800

    html

---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+function submit() {
+  const name = $('#name').val();
+  const sex = $('#sex').val();
+  const age = $('#age').val();
+  const mobile = $('#mobile').val();
+  $.ajax({
+    url: `http://47.92.141.244:3000/api/add?name=${name}&age=${age}&sex=${sex}&mobile=${mobile}`,
+    type: "GET",
+    success: (res) => {
+      refreshList();
+    }
+  });
+}
+
+function refreshList() {
+  $.ajax({
+    url: 'http://47.92.141.244:3000/api/getlist',
+    type: "GET",
+    success: (res) => {
+      if (res && res.code && res.code === 200 && res.data) {
+        let data = res.data.list;
+        let html = '';
+        data.forEach((val) => {
+          html += `<div>姓名:${val.name}-性别:${val.sex}-年龄:${val.age}-手机:${val.mobile}</div>`;
+        });
+        $('#list').html(html);
+      }
+    }
+  });
+}
