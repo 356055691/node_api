8849a42ec37129b4dbff2c278d52d416406d471f
李靖 <356055691@qq.com>
2020-06-30 12:20:09 +0800

    api

--- api/getlist.js
+++ api/getlist.js
@@ -2,29 +2,29 @@
 const mysql = require('mysql');
 
 exports.index = (req, res, next) => {
-    let connection = mysql.createConnection({
-      host     : '47.92.141.244',
-      user     : 'root',
-      password : '012399lee',
-      database : 'lijing'
-    });
-    
-    connection.connect();
- 
-    let  sql = 'SELECT * FROM users';
-    //查
-    connection.query(sql,function (err, result) {
-      if(err){
-        console.log('[SELECT ERROR] - ',err.message);
-        return;
+  let connection = mysql.createConnection({
+    host     : '47.92.141.244',
+    user     : 'root',
+    password : '012399lee',
+    database : 'lijing'
+  });
+  
+  connection.connect();
+
+  let  sql = 'SELECT * FROM users';
+  //查
+  connection.query(sql,function (err, result) {
+    if(err){
+      console.log('[SELECT ERROR] - ',err.message);
+      return;
+    }
+    res.json({
+      code: 200,
+      message: 'success',
+      data: {
+        list: result
       }
-      res.json({
-        code: 200,
-        message: 'success',
-        data: {
-          list: result
-        }
-      });
     });
-    connection.end();
+  });
+  connection.end();
 };
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const getlist = require(`../api/getlist`);
 const add = require(`../api/add`);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 router.get('/api/getlist', getlist.index);
 router.get('/api/add', add.index);
